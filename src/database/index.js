6b76fad97246324f94a2483442c02bee7ab3eea9
soul071213 <js071213asd@gmail.com>
2025-02-23 23:28:27 +0900

    마리아 db로 db 변경

--- src/database/index.js
+++ src/database/index.js
@@ -7,6 +7,7 @@ exports.pool=mysql.createPool(
         user:process.env.DB_USERNAME,
         password:process.env.DB_PASSWORD,
         database:process.env.DB_DATABASE,
+        port:process.env.DB_PORT,
         waitForConnections:true,
         connectionLimit:10,
         queueLimit:0
