9ff676b7acdaab0c38237f4d3f4afb063be19be2
soul071213 <js071213asd@gmail.com>
2025-02-26 19:05:01 +0900

    fix : 로그인 에러 수정

--- src/api/login/controller.js
+++ src/api/login/controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const repository = require('./repository');
+const bcrypt = require('bcryptjs');
 
 exports.login= async (req,res)=>{
     try{
@@ -7,24 +8,24 @@ exports.login= async (req,res)=>{
 
         //유저 조회
         const [users] = await repository.searchid(user_id);
+        // users를 []로 감싸는 이유는 배열인 값이 올 때 첫번째 값을 users에 할당 하기 위해서 [] 감싼다
         if(users.length===0){
             return res.status(400).json({message:"아이디가 존재하지 않습니다."});
         }
-
-        const user = user[0];
+        
+        // const user = users;
 
         //비밀번호 검증
-        const isMatch = await bcrypt.compare(password,user.password);
+        const isMatch = await bcrypt.compare(password,users.password);
         if(!isMatch) return res.status(400).json({message:"비밀번호가 일치 하지 않습니다."});
 
         //JWT 발급
-        const accessToken = jwt.sign({user_id:user.user_id},process.env.SECRETKEY,{expiresIn:'15m'});
-        const refreshToken = jwt.sign({user_id:user.user_id},process.env.SECRETKEY,{expiresIn:'7d'});
+        const accessToken = jwt.sign({user_id:users.user_id},process.env.SECRETKEY,{expiresIn:'15m'});
+        const refreshToken = jwt.sign({user_id:users.user_id},process.env.SECRETKEY,{expiresIn:'7d'});
 
         // Refresh Token을 쿠키에 저장
         res.cookie("refreshToken",refreshToken,{httpOnly:true,secure:false});
-        
-        res.json({accessToken});
+        res.json(accessToken);
     }catch(err){
         res.status(500).json({message:"서버 오류"});
     }
