8bb87eac7b455716009d378a84c6d97c8b0af325
soul071213 <js071213asd@gmail.com>
2025-03-04 00:44:40 +0900

    미들웨어 토큰 인증 코드 수정

--- src/middleware/authMiddleWare.js
+++ src/middleware/authMiddleWare.js
@@ -3,8 +3,7 @@ const jwt= require('jsonwebtoken');
 //Access Token 검증
 
 const authMiddleWare= (req,res,next)=>{
-    const token = req.header("Authorization")?.split(" ")[1];
-
+    const token = req.header("Authorization")?.split(" ")[0];
     if(!token) return res.status(401).json({meesage:"인증 실패 토큰 없음"});
 
     try{
